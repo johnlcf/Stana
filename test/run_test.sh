eb0af9646203b74683439d21df372dcdf1bd2c8e
johnlcf <johnlcf@gmail.com>
2018-06-13 22:20:13 +0800

    add a simple test

---
+++ test/run_test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# Just a very simple test that run all the plugins on all the files. We should improve this later and 
+# may be using a better framework.
+#
+for plugin in $(ls ../statPlugins/*.py | grep -v init | grep -v Base | sed 's#.*/\([^\.]*\).*#\1#'); do 
+	for file in $(ls *.out); do 
+		echo "Testing plugin $plugin on $file..."
+		../strace_analyser -e $plugin $file > /tmp/stana.output  # we should compare the output too
+		exit_val=$?
+		if [ ! $exit_val -eq 0 ]; then
+			exit 1
+		fi
+	done
+done
+
