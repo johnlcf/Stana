b8d9d0ab99c9a63ee1592a2216461d184deaa8a5
John Lau <johnlcf@gmail.com>
2018-06-22 16:39:07 +0000

    fix error commit

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,8 +3,7 @@
 # Just a very simple test that run all the plugins on all the files. We should improve this later and 
 # may be using a better framework.
 #
-#for plugin in $(ls ../statPlugins/*.py | grep -v init | grep -v Base | sed 's#.*/\([^\.]*\).*#\1#'); do 
-for plugin in StatFileIO; do 
+for plugin in $(ls ../statPlugins/*.py | grep -v init | grep -v Base | sed 's#.*/\([^\.]*\).*#\1#'); do 
 	for file in $(ls *.out); do 
 		echo "Testing plugin $plugin on $file..."
 		../strace_analyser -e $plugin $file > /tmp/${plugin}.${file}.output_old  # we should compare the output too
