e686d6f9fc7b298a17e5bf50177f92c536e041cd
John Lau <johnlcf@gmail.com>
2018-06-22 15:18:55 +0000

    Keep the output

--- test/run_test.sh
+++ test/run_test.sh
@@ -3,10 +3,11 @@
 # Just a very simple test that run all the plugins on all the files. We should improve this later and 
 # may be using a better framework.
 #
-for plugin in $(ls ../statPlugins/*.py | grep -v init | grep -v Base | sed 's#.*/\([^\.]*\).*#\1#'); do 
+#for plugin in $(ls ../statPlugins/*.py | grep -v init | grep -v Base | sed 's#.*/\([^\.]*\).*#\1#'); do 
+for plugin in StatFileIO; do 
 	for file in $(ls *.out); do 
 		echo "Testing plugin $plugin on $file..."
-		../strace_analyser -e $plugin $file > /tmp/stana.output  # we should compare the output too
+		../strace_analyser -e $plugin $file > /tmp/${plugin}.${file}.output_old  # we should compare the output too
 		exit_val=$?
 		if [ ! $exit_val -eq 0 ]; then
 			exit 1
